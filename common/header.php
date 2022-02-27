28186eede73c9e1993224a74367a438caad09727
InTostor <gena05irk@yandex.ru>
2022-02-27 18:10:12 +0800

    Перенос
    перенос фонов, созданы папки, добавлен header.php, импорт его в index.php

---
+++ common/header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML>
+<html lang="ru">
+
+<head>
+    <meta charset="utf-8">
+
+    <meta name="description" content="header" />
+    <link rel="stylesheet" href="../../styles/common/common.css">
+    <link rel="stylesheet" href="../../styles/common/header.css">
+</head>
+
+<header id="header" class="header">
+    <div class="header__backdrop">
+        <div class="container">
+
+            <div class="header__inner">
+
+                <a class="header__logo" href="index.html"> Project Explorers</a>
+                <nav class="nav">
+
+                    <a class="nav__link" href="about.html"> О нас</a>
+                    <a class="nav__link" href="projects.html"> Проекты</a>
+                    <a class="nav__link" href="shop.html">Магазин</a>
+                    <a class="nav__link" href="https://vk.com/projectexplorers"> Новости</a>
+                    <a class="nav__link" href="contacts.html"> Контакты</a>
+                </nav>
+
+                <div class="account">
+                    <image class="account__avatar"></image>
+                    <div class="account__inner">
+                        <a class="account__text" href="account.html">Instostoааааr</a>
+                        <a class="account__text" href="account.html">1000песо</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+</header>
+
---
+++ index.php
@@ -0,0 +1,20 @@
+<!-- Подключение верхней панели -->
+<?php
+include ('common/header.php');
+?>
+
+<!DOCTYPE HTML>
+<html lang="ru">
+
+<head>
+    <meta charset="utf-8">
+
+    <meta name="description" content="header" />
+    <link rel="stylesheet" href="../../styles/common/common.css">
+    <link rel="import" href="common/header.html">
+</head>
+
+
+
+<h1>ffffffffffffffffffffffffffffffffffff<h1>
+
